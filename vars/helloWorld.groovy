09903abfed0b123d335adde6b76153a7b67c1d28
parsuramkar <parsuramkar2014@gmail.om>
2023-06-21 17:53:55 +0530

    modified funcion

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,5 @@
-def call {
-	sh "echo Hello Wrld"
+def call(String name = 'human') {
+    // Any valid steps can be called from this code, just like in other
+    // Scripted Pipeline
+    echo "Hello, ${name}."
 }
